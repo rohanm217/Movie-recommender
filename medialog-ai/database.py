2459341c3f6063e6e30942dbe091f39d96cca107
Rohan Maligi <rmaligi@uci.edu>
2026-09-21 18:14:09 -0700

    opened connection to SQL database file and init_db creates the table if it hasn't been create already

--- medialog-ai/database.py
+++ medialog-ai/database.py
@@ -4,4 +4,27 @@ DB_name = "media_log.db"
 
 def get_connection():
     """Opens connection to the SQL Database file"""
+    return sqlite3.connect(DB_name)
 
+def init_db():
+    """Creates the table if it doesn't already exist"""
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS media (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            title TEXT NOT NULL,
+            media_type TEXT NOT NULL,
+            genre TEXT,
+            status TEXT NOT NULL, 
+            rating INTEGER,
+            notes TEXT,
+            created_at TEXT DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+    conn.commit()
+    conn.close()
+    print("Database ready.")
+
+if __name__ == "__main__":
+    init_db()
